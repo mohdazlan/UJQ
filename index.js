b89fd83bdcb37df66da40c336a7010a57198b16a
azlan <mhdazlanabaziz@gmai.com>
2020-06-23 22:02:27 +0800

    basic setup for static and ejs files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const fixedPORT = 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.set('layout', 'layouts/layout');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(expressLayouts);
+// this is for public folder on path /
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || fixedPORT, () => {
+    console.log(`listening to port ${process.env.PORT}`);
+  });
